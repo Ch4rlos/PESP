07c3e144395eca636f93ec09146ab2f38ee14221
Carlos Quiroga <quirogacarlos@gmail.com>
2020-02-21 13:15:07 -0600

    [Add] Country data is filling secondary view controller table

--- PESP/ViewController.swift
+++ PESP/ViewController.swift
@@ -8,36 +8,77 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
-    
     @IBOutlet weak var countryName: UILabel!
     var countryDetail : WelcomeElement?
     @IBOutlet weak var tableView: UITableView!
+    var datos : [String:Any] = [:]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         guard countryDetail != nil else {return}
         
-        let name = countryDetail?.name
+        let nombre = countryDetail?.name
+        let dominio = countryDetail?.cioc
+        let alfaCode = countryDetail?.alpha2Code
+        let codigoMarcacion = countryDetail?.numericCode
         let capital = countryDetail?.capital
+        let lenguajes = countryDetail?.languages
+        let region = countryDetail?.region
+        let subregion = countryDetail?.subregion
+        let poblacion = countryDetail?.population
+        let ubicacion = countryDetail?.latlng
+        let nacionalidad = countryDetail?.demonym
         let area = countryDetail?.area
-        let population = countryDetail?.population
-        let borders = countryDetail?.borders
-        let currencies = countryDetail?.currencies
-        let cioc = countryDetail?.cioc
-        let region = countryDetail?.region     
-
-        countryName.text = name
+        let zonasHorarias = countryDetail?.timezones
+        let fronteras = countryDetail?.borders
+        let nombreNativo = countryDetail?.nativeName
+        let códigoNumérico = countryDetail?.callingCodes
+        let moneda = countryDetail?.currencies
+        let traducciones = countryDetail?.translations
+        let imagenBandera = countryDetail?.flag
+        
+        countryName.text = nombre
+        
+        datos = ["Dominio":"\(dominio!)",
+         "Codigo Alfa":"\(alfaCode!)",
+         "Código Marcación":"\(codigoMarcacion!)",
+         "Capital":"\(capital!)",
+         "Lenguajes":"\(lenguajes!)",
+         "Region":"\(region!)",
+         "Subregion":"\(subregion!)",
+         "Poblacion":"\(poblacion!)",
+         "Ubicacion":"\(ubicacion!)",
+         "Nacionalidad":"\(nacionalidad!)",
+         "Área":"\(area!)",
+         "Zonas Horarias":"\(zonasHorarias!)",
+         "Fronteras":"\(fronteras!)",
+         "Nombre Nativo":"\(nombreNativo!)",
+         "Código Numérico":"\(códigoNumérico!)",
+         "Moneda":"\(moneda!)",
+         "Traducciones":"\(traducciones!)"]
         
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detallePais", for: indexPath)
+        
+        let key = Array(self.datos.keys)[indexPath.row]
+        
+        let value = Array(self.datos.values)[indexPath.row]
+     
 
+        
+        cell.textLabel?.text = key as! String
+        cell.detailTextLabel?.text = value as! String
+
+        
+        return cell
+    }
     /*
     // MARK: - Navigation
 
